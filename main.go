945815afb5d57e81dfb1468a9e9a6eb79d185694
Manuel Werder <manuelwerder@hotmail.com>
2026-03-02 13:21:40 +0100

    serving a 404 page

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var server http.Server
+
+	mux := http.NewServeMux()
+
+	server.Addr = ":8080"
+	server.Handler = mux
+
+	defer server.Close()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
 }
