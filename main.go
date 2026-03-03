570160d8b5657fe8c01357983da62c8c801dd06b
Manuel Werder <manuelwerder@hotmail.com>
2026-03-03 10:47:55 +0100

    testing handlerFunc

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +21,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
