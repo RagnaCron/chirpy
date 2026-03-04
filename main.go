eed568f28434a6003ae6368fd446c60d3a11b275
Manuel Werder <manuelwerder@hotmail.com>
2026-03-04 13:59:48 +0100

    Refactored main, so all handlers life in there own file

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -16,14 +14,18 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
 
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("POST /api/validate_chirp", vChirpHandler)
+
 	mux.HandleFunc("GET /admin/healthz", healthzHandler)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.hitsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
 	srv := &http.Server{
@@ -33,95 +35,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
-	hits := cfg.fileserverHits.Load()
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	hHTML := `
-<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>
-	`
-	_, _ = fmt.Fprintf(w, hHTML, hits)
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-}
-
-type msg struct {
-	Body string `json:"body"`
-}
-
-func vChirpHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	m := msg{}
-	err := decoder.Decode(&m)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "JSON decodeing went wrong")
-		return
-	}
-
-	if l := len(m.Body); l > 140 {
-		respondWithError(w, http.StatusBadRequest, "Chirp is to long.")
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, struct {
-		Valid bool `json:"valid"`
-	}{
-		Valid: true,
-	})
-
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	log.Printf("Error: %s\n", msg)
-	error := struct {
-		Error string `json:"error"`
-	}{
-		Error: msg,
-	}
-
-	data, err := json.Marshal(error)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s\n", err)
-		w.WriteHeader(code)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(data)
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	data, err := json.Marshal(payload)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Error marshalling JSON")
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(data)
-
 }
